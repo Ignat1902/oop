3407e5bdb5254c43775ec854ff0655587a66e324
Ignat1902 <soczkov.2013@gmail.com>
2020-10-17 15:54:09 +0300

    Раскидал все по папкам, все добавил

--- java labs/src/com/company/Task03.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Task03 {
-    public static void main(String[] args) {
-        int god = 2020-99;
-        System.out.println("МТУСИ основан в " + god + " году");
-    }
-}
