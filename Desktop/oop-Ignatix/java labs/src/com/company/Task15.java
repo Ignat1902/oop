fbfd985f0d14a8c61b0531e2cf165a971584263d
Ignatiks <55736900+Ignat1902@users.noreply.github.com>
2021-07-08 16:23:52 +0300

    Delete Desktop directory

--- Desktop/oop-Ignatix/java labs/src/com/company/Task15.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Task15 {
-    public static int number = 25;
-
-    public static void main(String[] args) {
-        System.out.println(number*number);
-    }
-}
